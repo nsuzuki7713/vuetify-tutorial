b2b6525cc72fd466bbaf0d695b634df1754b3d44
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-04-11 10:02:07 +0900

    カラー

---
+++ src/components/Color.vue
@@ -0,0 +1,21 @@
+// https://vuetifyjs.com/ja/styles/colors/
+<template>
+  <div>
+    <!-- buleでlighttenが1の色 -->
+    <div class="blue lighten-1 text-center">
+      <span class="white--text">Lorem ipsum</span>
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet,
+      <!-- 赤色のtextでlighttenが1にする -->
+      <strong class="red--text text--lighten-1">inciderint</strong> definitionem
+      est ea, explicari prodesset eam id. Mazim doctus vix an.
+      <span class="indigo--text text--darken-2"
+        >Amet causae probatus nec ex</span
+      >.
+    </div>
+    <div class="cyan text-center">
+      <span class="deep-orange--text">Lorem ipsum</span>
+    </div>
+  </div>
+</template>
